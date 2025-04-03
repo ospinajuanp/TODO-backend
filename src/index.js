286db46d687786ea864b6615412a21c9db8299e0
Ospinajuanp <juanpo996@gmail.com>
2025-04-03 17:40:35 -0500

    feature: test on vercel

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ connectDB() // Conectar a la base de datos
 // app.listen(3000) // Escuchar en el puerto 3000
 // console.log('Server running on port 3000') // Mostrar mensaje en la consola
 
-app.use('/api/hi', (req, res) => {
-    res.status(404).json({ status: 404, message: 'Not found' })
-})
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app
