f80a260e84ad73997efd7d2f1df57bb130bbac1e
Ospinajuanp <juanpo996@gmail.com>
2025-04-03 18:33:56 -0500

    feature: test on vercel 3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import app from './app.js'
 import { connectDB } from './db.js'
 
+const PORT = process.env.PORT || 3000;
+
 connectDB() // Conectar a la base de datos
-app.listen(3000) // Escuchar en el puerto 3000
-console.log('Server running on port 3000') // Mostrar mensaje en la consola
-
-
+// app.listen(3000) // Escuchar en el puerto 3000
+// console.log('Server running on port 3000') // Mostrar mensaje en la consola
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 export default app
