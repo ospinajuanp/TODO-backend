bafc8bf5f817c9f27cffe10e34f5304cf3311333
Ospinajuanp <juanpo996@gmail.com>
2025-04-03 18:29:21 -0500

    feature: test on vercel 2

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import app from './app.js'
 import { connectDB } from './db.js'
 
 connectDB() // Conectar a la base de datos
-// app.listen(3000) // Escuchar en el puerto 3000
-// console.log('Server running on port 3000') // Mostrar mensaje en la consola
+app.listen(3000) // Escuchar en el puerto 3000
+console.log('Server running on port 3000') // Mostrar mensaje en la consola
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
 
 export default app
