de7284baa19260d867df4391fe14ec17a651875d
Ospinajuanp <juanpo996@gmail.com>
2025-04-03 17:36:49 -0500

    feature: test on

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ connectDB() // Conectar a la base de datos
 // app.listen(3000) // Escuchar en el puerto 3000
 // console.log('Server running on port 3000') // Mostrar mensaje en la consola
 
+app.use('/api/hi', (req, res) => {
+    res.status(404).json({ status: 404, message: 'Not found' })
+})
+
 export default app
