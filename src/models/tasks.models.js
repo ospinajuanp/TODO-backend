51565ba4f83d61758c618ad1f2bb733b7275b53d
Ospinajuanp <juanpo996@gmail.com>
2025-04-09 09:49:18 -0500

    fixed tareas status automatico en pending

--- src/models/tasks.models.js
+++ src/models/tasks.models.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
     title: {type: String, required: true, trim: true},
     description: {type: String, required: true, trim: true},
+    status: {type: String, default: 'pending', required: true, trim: true},
     dateTarget: {type: Date, default: Date.now},
-    status: {type: String, required: true, trim: true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true}
 }, {
     timestamps: true // para que se agregue la fecha de creación y actualización
--- src/schemas/tasks.schema.js
+++ src/schemas/tasks.schema.js
@@ -7,8 +7,6 @@ export const createTasksSchema = z.object({
     description: z.string({
         required_error: 'Description is required',
     }).min(3),
-    status: z.string({
-        required_error: 'Status is required',
-    }).min(3),
+    status: z.string().min(3).optional(),
     dataTarget: z.string().datetime().optional()
 })
