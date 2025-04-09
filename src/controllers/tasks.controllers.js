4d7e3552dfba895b70112237c50327c66b675110
Ospinajuanp <juanpo996@gmail.com>
2025-04-09 16:59:47 -0500

    Feature agregando trycatch

--- src/controllers/tasks.controllers.js
+++ src/controllers/tasks.controllers.js
@@ -3,54 +3,76 @@ import User from '../models/user.model.js'
 
 // para obtener las tareas de la base de datos
 export const getTasks = async (req, res) => {
-    const tasks = await Tasks.find({
-        user: req.user.id // para buscar las tareas de un usuario
-    }).populate('user') // para popular el usuario 
-    res.json(tasks)
+    try {
+        const tasks = await Tasks.find({
+            user: req.user.id // para buscar las tareas de un usuario
+        }).populate('user') // para popular el usuario 
+        res.json(tasks)
+    } catch (error) {   
+        return res.status(500).json({ status: 500, message: 'Server error' })
+    }
 }
 
 // para obtener una tarea de la base de datos
 export const getTaskId = async (req, res) => {
-    const task = await Tasks.findById(req.params.id).populate('user')
-    if (!task) {
-        return res.status(404).json({ status: 404, message: 'Task not found' })
+    try {
+        const task = await Tasks.findById(req.params.id).populate('user')
+        if (!task) {
+            return res.status(404).json({ status: 404, message: 'Task not found' })
+        }
+        res.json(task)
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: 'Server error' })
     }
-    res.json(task)
 }
 
 // para crear una tarea en la base de datos de un usuario
 export const createTasks = async (req, res) => {
-    const {title, description, dateTarget, status } = req.body
-    const userFound = await User.findById(req.user.id) // para buscar el usuario
+    try {
+        const {title, description, dateTarget, status } = req.body
+        const userFound = await User.findById(req.user.id) // para buscar el usuario
 
-    const newTask = new Tasks({
-        title,
-        description,
-        dateTarget,
-        status,
-        user: userFound._id
-    })
+        const newTask = new Tasks({
+            title,
+            description,
+            dateTarget,
+            status,
+            user: userFound._id
+        })
 
-    const saveTask = await newTask.save()
-    res.json(saveTask)
+        const saveTask = await newTask.save()
+        res.json(saveTask)
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: 'Server error' })
+    }
     
 }
 
 // para actualizar una tarea
 export const updateTasks = async (req, res) => {
-    const task = await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    if (!task) {
-        return res.status(404).json({ status: 404, message: 'Task not found' })
+    try {
+        console.log(req.body)
+        const task = await Tasks.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+        if (!task) {
+            return res.status(404).json({ status: 404, message: 'Task not found' })
+        }
+        res.json(task)
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: 'Server error' })
     }
-    res.json(task)
 }
 
 // para eliminar una tarea
 export const deleteTasks = async (req, res) => {
-    const task = await Tasks.findByIdAndDelete(req.params.id)
+    try {
+        const task = await Tasks.findByIdAndDelete(req.params.id)
     
-    if (!task) {
-        return res.status(404).json({ status: 404, message: 'Task not found' })
+        if (!task) {
+            return res.status(404).json({ status: 404, message: 'Task not found' })
+        }
+        return res.json({ status: 204, message: 'Task deleted successfully' })
+    } catch (error) {
+        return res.status(500).json({ status: 500, message: 'Server error' })
     }
-    return res.json({ status: 204, message: 'Task deleted successfully' })
 }
